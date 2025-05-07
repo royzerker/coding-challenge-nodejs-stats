1f2bc2a9cd8d15af15dadd80eba29313206c8c17
Roy <inf.roycd@gmail.com>
2025-05-07 05:12:59 -0500

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import cors from 'cors'
 import 'dotenv/config'
 import express from 'express'
 import 'reflect-metadata'
+
 import swaggerUi from 'swagger-ui-express'
 
 import { registerDependencies } from './loaders/dependency-injector'
@@ -12,6 +14,7 @@ import { swaggerSpec } from './swagger'
 const app = express()
 const PORT = process.env.PORT || 8000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
