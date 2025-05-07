21ac003079cf4fb5086221873a107cdf44212040
Roy <inf.roycd@gmail.com>
2025-05-07 14:21:12 -0500

    chore: update

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,22 +1,23 @@
-import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerJsdoc from 'swagger-jsdoc'
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000
+const HOST = process.env.HOST || 'localhost'
 
-const options: any = { 
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'QR Factorization API',
-      version: '1.0.0',
-      description: 'API para factorizar matrices y realizar operaciones estadísticas.',
-    },
-    servers: [
-      {
-        url: `http://localhost:${PORT}/api`,
-      },
-    ],
-  },
-  apis: ['./src/routes/*.ts'], 
-};
+const options: any = {
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'QR Factorization API',
+			version: '1.0.0',
+			description: 'API para factorizar matrices y realizar operaciones estadísticas.'
+		},
+		servers: [
+			{
+				url: `http://${HOST}:${PORT}`
+			}
+		]
+	},
+	apis: ['./src/routes/*.ts']
+}
 
-export const swaggerSpec = swaggerJsdoc(options);
+export const swaggerSpec = swaggerJsdoc(options)
